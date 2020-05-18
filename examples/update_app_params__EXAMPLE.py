a74469dc42f7a7acd62ae3798d8caef519338777
Valley (US), Brandon A <brandon.a.valley@boeing.com>
2020-05-18 02:01:47 -0500

    in middle

--- examples/update_app_params__EXAMPLE.py
+++ examples/update_app_params__EXAMPLE.py
@@ -1,11 +1,42 @@
 # this file is ignored by the .gitignore to allow for param changes without repo update
 
+# path to top level file relative to this file
 TOP_LEVEL_FILE__REL_PATH = '..//main.py' 
-ICON__REL_PATH           = None                   # None for default python icon, must be .ico
-APP_DIR__REL_PATH        = 'app'                  # None for pwd
+
+# None for default python icon, must be .ico
+ICON__REL_PATH           = None             
+
+# None for pwd      
+APP_DIR__REL_PATH        = '..//app'                  
              
-DRY_RUN = False # set to True to see what cmd will be executed
+# can be paths to dirs or individual files
+# leave list empty to not copy anything extra into the dist dir
+COPY_INTO_DIST__INCLUDE_PATHS_L = ['..'] 
+
+
+# to exclude from any dir in COPY_INTO_DIST__INCLUDE_PATHS_L
+#    possible remove modes:  
+#                            'basename_equals'
+#                            'in_basename'      -- NOT IMPLEMENTED
+#                            'paths_equal'
+#                            'in_path'          -- NOT IMPLEMENTED
+#
+#        see path_l_remove() in file_system_utils for implemented / not implemented
+#
+# if COPY_INTO_DIST__INCLUDE_PATHS_L == [], this will be ignored 
+COPY_INTO_DIST__EXCLUDE_PATHS_LD = {'basename_equals' : ['.git', '__pycache__'],
+                                    'paths_equal'     : [APP_DIR__REL_PATH]}
+
+
+# set to True to see what cmd will be executed
+DRY_RUN = False 
+
+# delete the PYCACHE that pyinstaller creates
 DELETE_PYCACHE = True
+
+
+
+
 
 
 if __name__ == '__main__':
