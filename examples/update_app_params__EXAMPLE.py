c5e65e9aa7615fca758e3053e1d6aec343f6f56c
Valley (US), Brandon A <brandon.a.valley@boeing.com>
2020-05-18 02:17:41 -0500

    works

--- examples/update_app_params__EXAMPLE.py
+++ examples/update_app_params__EXAMPLE.py
@@ -1,17 +1,25 @@
 # this file is ignored by the .gitignore to allow for param changes without repo update
 
+import os
+
+# define everything as an abs path based on a relative path to this file so
+# this file can move in relation to update_app.py
+# NOT TESTED IF THIS GIVES PATH RELATIVE TO THIS FILE OR __main__
+def abs_path(in_path):
+    return os.path.abspath(in_path)
+
 # path to top level file relative to this file
-TOP_LEVEL_FILE__REL_PATH = '..//main.py' 
+TOP_LEVEL_FILE__REL_PATH = abs_path('..//main.py') 
 
 # None for default python icon, must be .ico
 ICON__REL_PATH           = None             
 
 # None for pwd      
-APP_DIR__REL_PATH        = '..//app'                  
+APP_DIR__REL_PATH        = abs_path('..//app')                  
              
 # can be paths to dirs or individual files
 # leave list empty to not copy anything extra into the dist dir
-COPY_INTO_DIST__INCLUDE_PATHS_L = ['..'] 
+COPY_INTO_DIST__INCLUDE_PATHS_L = [abs_path('..')] 
 
 
 # to exclude from any dir in COPY_INTO_DIST__INCLUDE_PATHS_L
@@ -24,8 +32,10 @@ COPY_INTO_DIST__INCLUDE_PATHS_L = ['..']
 #        see path_l_remove() in file_system_utils for implemented / not implemented
 #
 # if COPY_INTO_DIST__INCLUDE_PATHS_L == [], this will be ignored 
-COPY_INTO_DIST__EXCLUDE_PATHS_LD = {'basename_equals' : ['.git', '__pycache__'],
-                                    'paths_equal'     : [APP_DIR__REL_PATH]}
+COPY_INTO_DIST__EXCLUDE_PATHS_LD = {
+                                        'basename_equals' : ['.git', '__pycache__'],
+                                        'paths_equal'     : [abs_path(APP_DIR__REL_PATH)]
+                                    }
 
 
 # set to True to see what cmd will be executed
--- update_app.py
+++ update_app.py
@@ -59,43 +59,18 @@ def build_cmd():
     return cmd
 
 
+
+def copy_files_to_dist_dir():
+    # build init paths_to_copy_l before trimming
+    paths_to_copy_l = []
         
-# # works for single path str or list of paths
-# def delete_if_exists(path_str_or_l):
-#     def delete_single_fs_obj_fast(path):
-#         def onerror(func, path, exc_info):
-#             """
-#             Error handler for ``shutil.rmtree``.
-#         
-#             If the error is due to an access error (read only file)
-#             it attempts to add write permission and then retries.
-#         
-#             If the error is for another reason it re-raises the error.
-#         
-#             Usage : ``shutil.rmtree(path, onerror=onerror)``
-#             """
-#             import stat
-#             if not os.access(path, os.W_OK):
-#                 # Is the error an access error ?
-#                 os.chmod(path, stat.S_IWUSR)
-#                 func(path)
-#             else:
-#                 raise
-#             
-#         if os.path.exists(path):
-#             if   os.path.isdir(path):
-#                 shutil.rmtree(path, ignore_errors=False, onerror=onerror)
-#             elif os.path.isfile(path):
-#                 os.remove(path)
-#             else:
-#                 raise Exception('ERROR:  Gave something that is not a file or a dir, bad path: ', path)    
-#     
-#     
-#     if isinstance(path_str_or_l, str):
-#         path_str_or_l = [path_str_or_l]
-#     
-#     for path in path_str_or_l:
-#         delete_single_fs_obj_fast(path)
+#     for path in     
+    pass
+        
+        
+        
+        
+        
         
         
 
